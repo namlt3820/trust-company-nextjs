8e1745452505cf08126d4dd6a68ae2536503bc16
Le Trung Nam <namle@pegaxy.io>
2024-07-02 08:52:08 +0700

    feat: report component

---
+++ src/api/createReport.ts
@@ -0,0 +1,37 @@
+import { Comment } from '@/lib/payloadTypes'
+
+export type CreateComment = (params: CreateCommentParams) => Promise<Comment>
+
+export type CreateCommentParams = {
+  content: string
+  user: string
+  review: string
+}
+
+export const createComment: CreateComment = async (
+  params: CreateCommentParams
+) => {
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_PAYLOAD_CMS_SERVER_URL}/api/comments`,
+      {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(params),
+      }
+    )
+
+    if (res.ok) {
+      const { doc, errors } = await res.json()
+      if (errors) throw new Error(errors[0].message)
+      return doc
+    }
+
+    throw new Error('An error occurred while creating comment.')
+  } catch (e) {
+    throw new Error('An error occurred while creating comment.')
+  }
+}

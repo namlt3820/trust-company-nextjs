d8dc8899819255137d568cf33d76d63d510b1ed0
Le Trung Nam <namle@pegaxy.io>
2024-07-10 14:28:20 +0700

    feat: comment mobile

--- src/app/[locale]/comments/page.tsx
+++ src/app/[locale]/comments/page.tsx
@@ -22,54 +22,63 @@ export default function Comments() {
   const locale = useLocale()
 
   return (
-    <SectionWrapper backgroundColor="bg-white">
-      <SectionHeader title={t('Comment.title')} />
-      <CommentNavigation />
-      <div className="mx-auto w-full">
-        <ResourceStatus
-          isLoading={isLoading}
-          isNotFound={!isError && !isLoading && !commentsData?.docs.length}
-          isError={isError}
-          notFoundMessage={t('Comment.not_found')}
-        />
+    <div className="mt-24 md:mt-0">
+      {' '}
+      <SectionWrapper backgroundColor="bg-white">
+        <SectionHeader title={t('Comment.title')} />
+        <CommentNavigation />
+        <div className="mx-auto w-full">
+          <ResourceStatus
+            isLoading={isLoading}
+            isNotFound={!isError && !isLoading && !commentsData?.docs.length}
+            isError={isError}
+            notFoundMessage={t('Comment.not_found')}
+          />
 
-        <div className="grid gap-4">
-          {commentsData?.docs.map((comment) => {
-            const { id: commentId, user, content, updatedAt } = comment
-            const { name } = user as User
+          <div className="grid gap-4">
+            {commentsData?.docs.map((comment) => {
+              const { id: commentId, user, content, updatedAt } = comment
+              const { name } = user as User
 
-            return (
-              <Card key={commentId}>
-                <CardHeader>
-                  <div className="flex items-center justify-between">
-                    <div className="flex items-center space-x-3">
-                      <span className="text-lg font-semibold">{name}</span>
-                      <CommentReactions
-                        reactions={reactionsData?.find(
-                          ({ type, id }) =>
-                            type === 'comment' && id === commentId
-                        )}
-                        commentId={commentId}
-                      />
-                      <CommentActions comment={comment} />
+              return (
+                <Card key={commentId}>
+                  <CardHeader>
+                    <div className="flex flex-col items-start justify-between gap-4 md:flex-row md:items-center">
+                      <div className="grid w-full grid-cols-2 items-center gap-y-4 md:w-auto md:grid-cols-[auto,auto,auto] md:gap-x-2 md:gap-y-0">
+                        <span className="col-span-1 justify-self-start text-lg font-semibold">
+                          {name}
+                        </span>
+                        <div className="col-span-1 justify-self-end md:justify-self-start">
+                          <CommentReactions
+                            reactions={reactionsData?.find(
+                              ({ type, id }) =>
+                                type === 'comment' && id === commentId
+                            )}
+                            commentId={commentId}
+                          />
+                        </div>
+                        <div className="col-span-1 justify-self-start">
+                          <CommentActions comment={comment} />
+                        </div>
+                      </div>
+                      <span className="text-sm font-medium text-gray-500 md:text-base">
+                        {formatDate(new Date(updatedAt), locale as LocaleType)}
+                      </span>
                     </div>
-                    <span className="font-medium text-gray-500 dark:text-gray-400">
-                      {formatDate(new Date(updatedAt), locale as LocaleType)}
-                    </span>
-                  </div>
-                </CardHeader>
-                <CardContent>
-                  <p className="whitespace-pre-line font-medium text-gray-500 dark:text-gray-400">
-                    {content}
-                  </p>
-                </CardContent>
-              </Card>
-            )
-          })}
-        </div>
+                  </CardHeader>
+                  <CardContent>
+                    <p className="whitespace-pre-line font-medium text-gray-500 dark:text-gray-400">
+                      {content}
+                    </p>
+                  </CardContent>
+                </Card>
+              )
+            })}
+          </div>
 
-        <CommentPagination response={commentsData} />
-      </div>
-    </SectionWrapper>
+          <CommentPagination response={commentsData} />
+        </div>
+      </SectionWrapper>
+    </div>
   )
 }

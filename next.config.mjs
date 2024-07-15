3d9c0d2d8b9f82657b0db8bb8112044d025ee5c3
Le Trung Nam <namle@pegaxy.io>
2024-07-15 07:56:29 +0700

    fix: remotePattern production

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,23 @@
 import createNextIntlPlugin from 'next-intl/plugin'
 
 const withNextIntl = createNextIntlPlugin()
-
 const payloadUrl = new URL(process.env.NEXT_PUBLIC_PAYLOAD_URL_FROM_SERVER)
 
 /** @type {import('next').NextConfig} */
+
+const remotePattern = {
+  protocol: payloadUrl.protocol.slice(0, -1),
+  hostname: payloadUrl.hostname,
+  pathname: '/media/**',
+}
+
+if (process.env.NODE_ENV !== 'production') {
+  remotePattern.port === '3000'
+}
+
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: payloadUrl.protocol.slice(0, -1),
-        hostname: payloadUrl.hostname,
-        port: '3000',
-        pathname: '/media/**',
-      },
-    ],
+    remotePatterns: [remotePattern],
   },
 }
 
